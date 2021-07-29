5305f9c88b735cba84bf2a1c8d479c80409a1f39
krry <krryxq@163.com>
2021-07-29 22:35:39 +0800

    feat(api): add composition api

---
+++ pages/krry/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div>
+      <ul>
+        <li v-for="ele in eleList" :key="ele.id">{{ ele.name }}</li>
+      </ul>
+      <button @click="addEle">添加</button>
+    </div>
+    <div>
+      <ul>
+        <li v-for="ele in dataObj.todoList" :key="ele.id">{{ ele.name }}</li>
+      </ul>
+      <button @click="addTodo">添加</button>
+    </div>
+  </div>
+</template>
+<script>
+import { ref, reactive, defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {
+    // ref
+    const eleList = ref([])
+    function addEle() {
+      const len = eleList.value.length
+      eleList.value.push({
+        id: len,
+        name: 'ref 自增' + len,
+      })
+    }
+    
+    // reactive
+    const dataObj = reactive({
+      todoList: [],
+    })
+    function addTodo() {
+      const len = dataObj.todoList.length
+      dataObj.todoList.push({
+        id: len,
+        name: 'reactive 自增' + len,
+      })
+    }
+
+    return {
+      eleList,
+      addEle,
+      dataObj,
+      addTodo
+    }
+  }
+})
+</script>
